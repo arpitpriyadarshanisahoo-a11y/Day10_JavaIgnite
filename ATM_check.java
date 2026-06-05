368e8a68320703a7e5ee5ca53f1115c25ba0f807
arpitpriyadarshanisahoo-a11y <arpitpriyadarshanisahoo@gmail.com>
2026-06-05 17:45:32 +0530

    Update ATM_check.java

--- ATM_check.java
+++ ATM_check.java
@@ -1,13 +1,57 @@
-/*
-ATM Multi-Level Exception System
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
-Create a program that simulates ATM withdrawal.
+class InsufficientBalanceException extends Exception {
+    InsufficientBalanceException(String msg) {
+        super(msg);
+    }
+}
 
-Requirements:
-Take balance and withdrawal amount
-If withdrawal > balance → handle exception
-If withdrawal amount is negative → handle exception
-If input is invalid (text instead of number) → handle exception
-Expected Output:
-Transaction Successful / Insufficient Balance / Invalid Input
-*/
+class NegativeAmountException extends Exception {
+    NegativeAmountException(String msg) {
+        super(msg);
+    }
+}
+public class ATMExceptionDemo {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        try {
+	            System.out.print("Enter Account Balance: ");
+	            double balance = sc.nextDouble();
+
+	            System.out.print("Enter Withdrawal Amount: ");
+	            double withdraw = sc.nextDouble();
+
+	            if (withdraw < 0) {
+	                throw new NegativeAmountException("Withdrawal amount cannot be negative.");
+	            }
+
+	            if (withdraw > balance) {
+	                throw new InsufficientBalanceException("Insufficient Balance.");
+	            }
+
+	            balance = balance - withdraw;
+
+	            System.out.println("Transaction Successful");
+	            System.out.println("Remaining Balance: " + balance);
+	        }
+
+	        catch (InputMismatchException e) {
+	            System.out.println("Invalid Input");
+	        }
+
+	        catch (NegativeAmountException e) {
+	            System.out.println(e.getMessage());
+	        }
+
+	        catch (InsufficientBalanceException e) {
+	            System.out.println(e.getMessage());
+	        }
+
+	        finally {
+	            sc.close();
+	        }
+	    }
+
+}
