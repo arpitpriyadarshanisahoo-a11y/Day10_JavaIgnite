7c690e230870f027e5b893ba273e42016fcaf67c
arpitpriyadarshanisahoo-a11y <arpitpriyadarshanisahoo@gmail.com>
2026-06-05 16:59:09 +0530

    Update Student_Marks.java

--- Student_Marks.java
+++ Student_Marks.java
@@ -1,12 +1,31 @@
-/*
-Student Marks System (Safe Input)
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
-Create a program that:
 
-Stores 5 student marks in an array
-Takes index input from user
-Displays mark
-Add Exception Handling:
-Handle invalid index
-Handle invalid input (if user enters text instead of number)
-*/
+public class StudentMarksSystem {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int marks[] = {85, 90, 78, 88, 95};
+
+        try {
+            System.out.print("Enter index (0-4): ");
+            int index = sc.nextInt();
+
+            System.out.println("Mark = " + marks[index]);
+        }
+
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid Index");
+        }
+
+        catch (InputMismatchException e) {
+            System.out.println("Invalid Input");
+        }
+
+        finally {
+            sc.close();
+        }
+    }
+
+}
